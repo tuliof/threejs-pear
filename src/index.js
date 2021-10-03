3a16cdcab2cf82ca0324e9a4f16ebc3c68ba7344
Tulio Fernandes <tulio.fernandes@gmail.com>
2021-10-03 19:33:44 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import * as THREE from "three";
+
+// const canvas = document.getElementById("c");
+// const renderer = new THREE.WebGLRenderer({ canvas });
+// renderer.setSize(window.innerWidth, window.innerHeight);
+
+// const camera = new THREE.PerspectiveCamera(
+//   45,
+//   window.innerWidth / window.innerHeight,
+//   1,
+//   500
+// );
+// camera.position.set(0, 0, 100);
+// camera.lookAt(0, 0, 0);
+
+// const scene = new THREE.Scene();
+
+function main() {
+  const canvas = document.querySelector("#c");
+  const renderer = new THREE.WebGLRenderer({ canvas });
+
+  const fov = 75;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
+
+  const scene = new THREE.Scene();
+
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 }); // greenish blue
+
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  function render(time) {
+    time *= 0.001; // convert time to seconds
+
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+}
+
+main();
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'src/index.html' }],
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
+
+module.exports = config;
